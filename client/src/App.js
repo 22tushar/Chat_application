1c938ef0c6d3c29c7c0eab9289c706a61e351bb5
22tushar <gehlottushar18@gmal.com>
2022-05-23 11:22:06 +0530

    add server url

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://chat-application-server.vercel.app/");
 
 function App() {
   //Room State
