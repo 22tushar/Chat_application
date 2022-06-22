57522c9401efbdcc08705cf4382f51a6b8451330
22tushar <gehlottushar18@gmal.com>
2022-06-22 18:32:32 +0530

    fix:env

--- server/index.js
+++ server/index.js
@@ -5,9 +5,11 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 const dotenv=require('dotenv')
 app.use(cors());
-app.use("dotenv")
+// app.use("dotenv")
 const server = http.createServer(app);
 dotenv.config()
+// aa
+const PORT = process.env.PORT || 5000
 const io = new Server(server, {
   cors: {
     origin: "*",    
@@ -27,6 +29,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT, () => {
+server.listen( PORT , () => {
   console.log("SERVER IS RUNNING");
 });
